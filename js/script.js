b5dacf6704c2e99e15333e83c3650e9a87abef43
Ingo <inkyfischer@gmail.com>
2020-04-28 21:13:38 +0200

    creating category pages

--- js/script.js
+++ js/script.js
@@ -1,18 +1,12 @@
 "use strict"
-
-
-
-
-
-
-
 /* ********************************************************************* HEADER ********************************************************************** */
 async function getHeader() {
-    let response = await fetch('./temp/header.html')
+    let response = await fetch('/temp/header.html')
     if(response.ok) {
         let data = await response.text()
         let header = document.querySelector('header')
         header.innerHTML = data
+        /*
         let sportswear = document.querySelector('[name="sportswear"]')
         sportswear.addEventListener('click', e => {
             console.log(e);
@@ -22,25 +16,11 @@ async function getHeader() {
         shoes.addEventListener('click', e => {
             console.log(e);
         getSportswear(e)  
+        })
+        */
     } else {
         console.log('Connect Error');
     }
 }
 
 getHeader()
-
-/* ******************************************************************** PRODUCTS ******************************************************************** */
-function getProducts() {
-    return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest()
-        xhr.open('GET', '/data.json')
-        xhr.send()
-        xhr.onload = () => {          
-            if(xhr.status == 200){
-                resolve(xhr.response)
-            } else {
-                reject('The products could not been found.')
-            }
-        }
-    })
-}
