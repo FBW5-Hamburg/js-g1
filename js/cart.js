860c8d1d9536568a1fecbf87b8b93e7b5eb928fb
Ingo <inkyfischer@gmail.com>
2020-04-29 12:58:12 +0200

    clear amount of products page

--- js/cart.js
+++ js/cart.js
@@ -1,31 +1,4 @@
 "use strict"
-
-/* ********************************************************************* HEADER ********************************************************************** */
-async function getHeader() {
-    let response = await fetch('./temp/header.html')
-    if(response.ok) {
-        let data = await response.text()
-        let header = document.querySelector('header')
-        header.innerHTML = data
-    //     let sportswear = document.querySelector('[name="sportswear"]')
-    //     sportswear.addEventListener('click', e => {
-    //         console.log(e);    
-    //     })
-    //     getSportswear()
-    // } else {
-    //     console.log('Connect Error');
-    }
-}
-
-getHeader()
-
-
-
-
-
-
-
-
 /* ************************** CART ************************ */
 window.onload = () => {
 
@@ -167,4 +140,4 @@ window.onload = () => {
 }
     //let jsonObjFinal = localStorage.getItem('cartlist')
         
-    };
+};
